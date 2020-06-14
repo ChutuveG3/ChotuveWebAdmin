7f7bdde396c3fbab8021fbdd8ef50467ad87ac67
Santiago Almejun <santi.alme@gmail.com>
2020-06-14 09:17:15 -0300

    testing prod dockerfile release 8

--- src/components/CountDisplay.js
+++ src/components/CountDisplay.js
@@ -8,7 +8,7 @@ export default class CountDisplay extends Component{
     render() {
         return (
             <div className="users-count">
-                <h1 style={{fontSize: '400%', fontStyle: 'bold'}}>{this.state.count}</h1>
+                <h1 style={{fontSize: '400%'}}>{this.state.count}</h1>
                 <p>Total users in the site</p>
             </div>
         );
